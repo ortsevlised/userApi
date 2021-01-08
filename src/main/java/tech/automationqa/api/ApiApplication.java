a6dc2a91c4cf0da78ae194b692c5169caa4b8806
Jorge <jdesilvestro@gmail.com>
2021-01-08 13:08:38 +0000

    Starting the h2 db with tcp

--- src/main/java/tech/automationqa/api/ApiApplication.java
+++ src/main/java/tech/automationqa/api/ApiApplication.java
@@ -1,13 +1,40 @@
 package tech.automationqa.api;
 
+import lombok.extern.java.Log;
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import java.sql.SQLException;
 
+@Log
 @SpringBootApplication
-public class ApiApplication {
+
+public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		startH2Server();
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		startH2Server();
+		return application.sources(ApiApplication.class);
+	}
+
+	private static void startH2Server() {
+		try {
+
+			Server h2Server = Server.createTcpServer().start();
+			if (h2Server.isRunning(true)) {
+				log.info("H2 server was started and is running.");
+			} else {
+				throw new RuntimeException("Could not start H2 server.");
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Failed to start H2 server: ", e);
+		}
+	}
 }
